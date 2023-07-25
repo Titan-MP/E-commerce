f8caf14c142d77e9e2e1a140d0f83d13c9ba1a75
Titan-MP <marcrhymaun@knights.ucf.edu>
2023-07-24 21:59:20 -0400

    fixed and completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,16 +7,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({include:[Category,Tag]}).then((productData) => {
+    res.json(productData);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({where:{id:req.params.id},include:[Category,Tag]}).then((productData) => {
+    res.json(productData);
+  })
 });
 
 // create new product
 router.post('/', (req, res) => {
+
+  // Product.create(req.body)
+  //   .then((product) => {
+  //     res.json(product);
+  //   })
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -94,6 +105,9 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({where:{id:req.params.id}}).then((productData) => {
+    res.json(productData);
+  })
 });
 
 module.exports = router;
